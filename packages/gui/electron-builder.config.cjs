33c69279141d0046fc27501db26c8f85cf23d2dd
atomlong <atom.long@outlook.com>
2026-05-21 19:56:45 +0800

    fix: stabilize github actions packaging
    (cherry picked from commit 38d81125e688ec5ab7b97879ec60db8e9cf66b67)

--- packages/gui/electron-builder.config.cjs
+++ packages/gui/electron-builder.config.cjs
@@ -88,6 +88,8 @@ const linuxTargets = [
   },
 ]
 
+const enableFlatpak = process.env.DEV_SIDECAR_ENABLE_FLATPAK === '1'
+
 if (hasExecutable('rpmbuild')) {
   linuxTargets.push({
     target: 'rpm',
@@ -95,7 +97,7 @@ if (hasExecutable('rpmbuild')) {
   })
 }
 
-if (hasExecutable('flatpak') && hasExecutable('flatpak-builder')) {
+if (enableFlatpak && hasExecutable('flatpak') && hasExecutable('flatpak-builder')) {
   linuxTargets.push({
     target: 'flatpak',
     arch: ['x64'],
