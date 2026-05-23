b00ad20cf63bf8b1f57af64355f21d0338f2fef1
atomlong <atom.long@outlook.com>
2026-05-23 11:19:41 +0800

    fix packaged gui mitmproxy runtime path
    (cherry picked from commit 466372fe5988f2a071acede10c5d626517a08922)

--- packages/gui/src/bridge/mitmproxy.js
+++ packages/gui/src/bridge/mitmproxy.js
@@ -1,11 +1,9 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import server from '@docmirror/mitmproxy'
 import jsonApi from '@docmirror/mitmproxy/src/json.js'
 import log from '@docmirror/mitmproxy/src/utils/util.log.server.js' // 当前脚本是在 server 的进程中执行的，所以使用 mitmproxy 中的logger
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const configPath = process.argv[2]
 const configJson = fs.readFileSync(configPath)
 log.info('读取 running.json by gui bridge 成功:', configPath)
@@ -20,6 +18,33 @@ try {
 // config.setting.script.defaultDir = path.join(__dirname, scriptDir)
 // const pacFilePath = '../extra/pac/pac.txt'
 // config.plugin.overwall.pac.customPacFilePath = path.join(__dirname, pacFilePath)
-config.setting.rootDir = path.join(__dirname, '../')
+if (!config.setting) {
+  config.setting = {}
+}
+config.setting.rootDir = resolveRootDir()
+log.info('resolved gui bridge rootDir:', config.setting.rootDir)
 log.info(`start mitmproxy by gui bridge, configPath: ${configPath}`)
 server.start(config)
+
+function resolveRootDir () {
+  const candidates = []
+
+  if (process.argv[1]) {
+    candidates.push(path.resolve(path.dirname(process.argv[1]), '../'))
+  }
+
+  if (process.env.DS_EXTRA_PATH) {
+    candidates.push(path.resolve(process.env.DS_EXTRA_PATH, '../'))
+  }
+
+  candidates.push(path.resolve(process.cwd(), 'dist_electron'))
+  candidates.push(process.cwd())
+
+  for (const candidate of candidates) {
+    if (fs.existsSync(path.join(candidate, 'extra'))) {
+      return candidate
+    }
+  }
+
+  return candidates[0]
+}
